7db0c17e7a37253586d33499f0ffbded9355887e
Sebastian Werner <sebastian.werner@saltendorf.com>
2022-01-03 04:22:16 +0100

    Day 10 Part 2

--- aoc_d10_p1.py
+++ aoc_d10_p1.py
@@ -4,6 +4,7 @@ input_file = open('aoc_d10_in.txt')
 OPEN_SYMBOLS = {'(': 1, '[': 2, '{': 3, '<': 4}
 CLOSE_SYMBOLS = {')': 1, ']': 2, '}': 3, '>': 4}
 
+
 id_counter = 0
 input_dict = {}
 
@@ -12,6 +13,7 @@ for line in input_file:
     id_counter += 1
 
 corruption_dict = {')': 0, ']': 0, '}': 0, '>': 0}
+corruption_list = []
 for key in input_dict.keys():
     open_list = []
     for symbol in input_dict[key]:
@@ -25,6 +27,7 @@ for key in input_dict.keys():
                 corruption_dict[symbol] += 1
                 # print(f'For Key {key} the corrupt symbol is {symbol}')
                 # print(open_list)
+                corruption_list.append(key)
                 break
             else:
                 print('Error Close Check')
@@ -36,4 +39,38 @@ corruption_score += corruption_dict['}'] * 1197
 corruption_score += corruption_dict['>'] * 25137
 
 # print(corruption_dict)
-print(f'\nThe final Score is {corruption_score}')
+# print(f'\nThe final Score is {corruption_score}')
+
+for corruption_key in corruption_list:
+    del input_dict[corruption_key]
+
+
+completion_list = []
+for key in input_dict.keys():
+    open_list = []
+    for symbol in input_dict[key]:
+        if symbol in OPEN_SYMBOLS.keys():
+            open_list.append(OPEN_SYMBOLS[symbol])
+
+        elif symbol in CLOSE_SYMBOLS.keys():
+            if CLOSE_SYMBOLS[symbol] == open_list[-1]:
+                open_list.pop()
+
+            else:
+                print('Error Close Check')
+
+    completion_score = 0
+    for order in range(-1, -1 - len(open_list), -1):
+        completion_score *= 5
+        completion_score += open_list[order]
+
+    completion_list.append(completion_score)
+
+completion_list.sort()
+
+while len(completion_list) > 1:
+    completion_list.pop()
+    completion_list.pop(0)
+
+print(completion_list[0])
+
